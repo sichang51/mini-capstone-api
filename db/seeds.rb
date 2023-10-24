66700e121aa4f2fec379d251a9c16bf1c99221f1
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-10-24 16:54:21 -0700

    added seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.create(product: "Video Game Console", name: "Xbox Series X", price: 600, description: "plays xbox series x games", image_url: "url placeholder")
+
+Product.create(product: "Video Game Console", name: "Playstation 5", price: 600, description: "plays ps5 games", image_url: "url placeholder")
+
+Product.create(product: "Video Game", name: "Madden 2024", price: 70, description: "football game", image_url: "url placeholder")
+
+Product.create(product: "Video Game", name: "Call of Duty: Modern Warfare", price: 70, description: "first person shooter warfare", image_url: "url placeholder")
+
+Product.create(product: "Video Game Accessories", name: "Xbox Series X Controller", price: 90, description: "controller for Xbox Series X", image_url: "url placeholder")
+
+Product.create(product: "Video Game Accessories", name: "PS5 Controller", price: 90, description: "controller for PS5", image_url: "url placeholder")

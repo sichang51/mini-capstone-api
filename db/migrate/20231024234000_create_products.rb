a574541c79e9dff70d1d8d84082f3438a513f24c
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-10-24 16:40:39 -0700

    added model and attributes

---
+++ db/migrate/20231024234000_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :product
+      t.string :name
+      t.integer :price
+      t.string :description
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end

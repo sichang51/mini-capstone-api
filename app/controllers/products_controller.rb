ec7f417d9713932c978a40b436542d02720b0dae
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-10-24 17:44:28 -0700

    added more details to product descripton

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
   def all_products
     all_products = Product.all
-    render json: { product: all_products }
+    render json: { all_products: all_products }
   end
 
   def first_product
     first_product = Product.first
-    render json: { product: first_product }
+    render json: { first_product: first_product }
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,15 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Product.create(product: "Video Game Console", name: "Xbox Series X", price: 600, description: "plays xbox series x games", image_url: "url placeholder")
+Product.create(product: "Video Game Console", name: "Xbox Series X", price: 560, description: "Xbox Series X, the fastest, most powerful Xbox ever. Explore rich new worlds with 12 teraflops of raw graphic processing power, DirectX ray tracing, a custom SSD, and 4K gaming. Make the most of every gaming minute with Quick Resume, lightning-fast load times, and gameplay of up to 120 FPS—all powered by Xbox Velocity Architecture. Enjoy thousands of games from four generations of Xbox, with hundreds of optimized titles that look and play better than ever. Get the most out of your Xbox Series X with Xbox Game Pass Ultimate (membership sold separately) and be the first to play new games like Starfield and Forza Motorsport on day one. Plus, enjoy hundreds of games like Minecraft Legends, Halo Infinite, and Forza Horizon 5 with friends on console, PC, and cloud. With games added all the time, there’s always something new to play.", image_url: "url placeholder")
 
-Product.create(product: "Video Game Console", name: "Playstation 5", price: 600, description: "plays ps5 games", image_url: "url placeholder")
+Product.create(product: "Video Game Console", name: "Playstation 5", price: 500, description: "
+Powered by an eight-core AMD Zen 2 CPU and custom AMD Radeon GPU, the PS5 is offered in two models: with and without a 4K Blu-ray drive. Supporting a 120Hz video refresh, the PS5 is considerably more powerful than the PS4 and PS4 Pro. Rather than GDDR5 memory, GDDR6 is supported with capacity doubled from 8 to 16GB.", image_url: "url placeholder")
 
-Product.create(product: "Video Game", name: "Madden 2024", price: 70, description: "football game", image_url: "url placeholder")
+Product.create(product: "Video Game", name: "Madden NFL 2024", price: 70, description: "Madden NFL 24 introduces exclusive next gen features, including SAPIEN technology, which allows for more realistic movement and more lifelike player models. Gameplay includes the return of refs to the field, improved blocking and Fieldsense.", image_url: "url placeholder")
 
-Product.create(product: "Video Game", name: "Call of Duty: Modern Warfare", price: 70, description: "first person shooter warfare", image_url: "url placeholder")
+Product.create(product: "Video Game", name: "Call of Duty: Modern Warfare 2", price: 70, description: "Call of Duty: Modern Warfare 2 is a first person shooter, and its gameplay revolves around fast-paced gunfights against enemy combatants. The player controls a soldier who can perform several actions, including jump, sprint, crouch, lay prone, and aim down their gun's iron sights.", image_url: "url placeholder")
 
-Product.create(product: "Video Game Accessories", name: "Xbox Series X Controller", price: 90, description: "controller for Xbox Series X", image_url: "url placeholder")
+Product.create(product: "Video Game Accessories", name: "Xbox Series X Controller", price: 50, description: "For Xbox Series X|S, we’ve streamlined the Xbox Wireless Controller for comfort, performance, and instant sharing. The controller also works great with Xbox One, Windows 10/11, and cloud gaming devices.", image_url: "url placeholder")
 
-Product.create(product: "Video Game Accessories", name: "PS5 Controller", price: 90, description: "controller for PS5", image_url: "url placeholder")
+Product.create(product: "Video Game Accessories", name: "PS5 Controller", price: 70, description: "The DualSense wireless controller offers immersive haptic feedback, dynamic adaptive triggers and a built-in microphone, all integrated into an iconic comfortable design. Compared to DUALSHOCK 4 wireless controller. Available when feature is supported by game.", image_url: "url placeholder")

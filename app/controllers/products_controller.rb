a20dd0fefc3a81f43260194fa1e92cd2064454f9
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-10-24 17:03:40 -0700

    added JSON to controller and routes to display ALL products and first

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+  def all_products
+    all_products = Product.all
+    render json: { product: all_products }
+  end
+
+  def first_product
+    first_product = Product.first
+    render json: { product: first_product }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/all_products", controller: "products", action: "all_products"
+
+  get "/first_product", controller: "products", action: "first_product"
 end

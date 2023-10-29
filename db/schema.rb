827e67dc62333ec2d97bd596b680847c799ddf6b
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-10-29 10:03:38 -0700

    added inventory column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_29_165953) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_29_170157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_29_165953) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "inventory"
   end
 
 end

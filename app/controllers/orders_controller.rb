2a02590d73400bf9d44cb220139669adc09471fc
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-11-14 16:23:31 -0800

    added method to orders_controllers.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,17 @@
 class OrdersController < ApplicationController
+  def create
+    @order = Order.create(
+      user_id: params[:user_id],
+      product_id: params[:product_id],
+      name: params["name"],
+      price: params["price"],
+      description: params["description"],
+      inventory: params["inventory"],
+      supplier_id: params[:supplier_id],
+      quantity: params["quantity"],
+      subtotal: params[:subtotal],
+      tax: params[:tax],
+      total: params[:total],
+    )
+  end
 end

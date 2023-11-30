fa5ed0168cf91adfee897eba1b1263d6881edb9e
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-11-29 18:02:46 -0800

    added destroy function to carted_products_controller.rb:

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -20,4 +20,10 @@ class CartedProductsController < ApplicationController
       render json: { errors: @carted_product.errors.full_messages }, status: 422
     end
   end
+
+  def destroy
+    carted_product = current_user.carted_products.find_by(id: params[:id], status: "carted")
+    carted_product.update(status: "removed")
+    render json: { status: "Carted item removed" }
+  end
 end

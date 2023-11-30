96bb17355c7b851a8e6e2c35af8dd2d49b81aecc
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-11-29 18:08:03 -0800

    added destroy route to carted_products in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -38,5 +38,5 @@ Rails.application.routes.draw do
   # carted_products table
   get "/carted_products" => "carted_products#index"
   post "/carted_products" => "carted_products#create"
-  
+  delete "/carted_products/:id" => "carted_products#destroy"
 end
